5056a7e08c2e28d765d027c42cf84435ab271dd4
sujiinnn <sujiinnn@gmail.com>
2025-06-20 17:15:53 +0900

    마이페이지 조회 + JWT

--- src/main/java/com/example/ddukdoc/repository/QnARepository.java
+++ src/main/java/com/example/ddukdoc/repository/QnARepository.java
@@ -1,7 +1,11 @@
 package com.example.ddukdoc.repository;
 
+import com.example.ddukdoc.entity.Member;
 import com.example.ddukdoc.entity.QnA;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QnARepository extends JpaRepository<QnA, Integer> {
+    List<QnA> findAllByWriter(Member member);
 }
