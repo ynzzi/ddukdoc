05757fe7e6977ac79f3af905885a829446044190
sujiinnn <sujiinnn@gmail.com>
2025-06-23 03:55:30 +0900

    문의 등록

--- src/main/java/com/example/ddukdoc/DdukdocApplication.java
+++ src/main/java/com/example/ddukdoc/DdukdocApplication.java
@@ -4,8 +4,10 @@ import jakarta.persistence.EntityListeners;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EntityListeners(AuditingEntityListener.class)
+@EnableJpaAuditing
 @SpringBootApplication
 public class DdukdocApplication {
 
